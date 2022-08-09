a126d5dbe985868844d599a4a7f365dabde09ec9
URK21CO2003 <110015503+URK21CO2003@users.noreply.github.com>
2022-08-09 18:30:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,41 +1,58 @@
-class evalpostfix:
-    def __init__(self):
-        self.stack =[]
-        self.top =-1
+class Evaluate:
+      
+    # Constructor to initialize the class variables
+    def __init__(self, capacity):
+        self.top = -1
+        self.capacity = capacity
+        # This array is used a stack 
+        self.array = []
+      
+    # check if the stack is empty
+    def isEmpty(self):
+        return True if self.top == -1 else False
+      
+    # Return the value of the top of the stack
+    def peek(self):
+        return self.array[-1]
+      
+    # Pop the element from the stack
     def pop(self):
-        if self.top ==-1:
-            return
+        if not self.isEmpty():
+            self.top -= 1
+            return self.array.pop()
         else:
-            self.top-= 1
-            return self.stack.pop()
-    def push(self, i):
-        self.top+= 1
-        self.stack.append(i)
+            return "$"
+      
+    # Push the element to the stack
+    def push(self, op):
+        self.top += 1
+        self.array.append(op) 
   
-    def centralfunc(self, ab):
-        for i in ab:
   
-            # if the component of the list is an integer
-            try:
-                self.push(int(i))
-            # if the component of the list is not an integer, 
-            # it must be an operator. Using ValueError, we can 
-            # evaluate components of the list other than type int
-            except ValueError:
+    # The main function that converts given infix expression
+    # to postfix expression
+    def evaluatePostfix(self, exp):
+          
+        # Iterate over the expression for conversion
+        for i in exp:
+              
+            # If the scanned character is an operand
+            # (number here) push it to the stack
+            if i.isdigit():
+                self.push(i)
+  
+            # If the scanned character is an operator,
+            # pop two elements from stack and apply it.
+            else:
                 val1 = self.pop()
                 val2 = self.pop()
-                if i == '/':
-                  self.push(val2 / val1)
-                else:        
-                # switch statement to perform operation
-                  switcher ={'+':val2 + val1, '-':val2-val1, '*':val2 * val1, '^':val2**val1}
-                  self.push(switcher.get(i))
+                self.push(str(eval(val2 + i + val1)))
+  
         return int(self.pop())
+                  
   
-str ='100 200 + 2 / 5 * 7 +'
-  
-# splitting the given string to obtain 
-# integers and operators into a list
-strconv = str.split(' ')
-obj = evalpostfix()
-print(obj.centralfunc(strconv))
+              
+# Driver program to test above function
+exp = "231*+9-"
+obj = Evaluate(len(exp))
+print ("postfix evaluation: %d"%(obj.evaluatePostfix(exp)))
