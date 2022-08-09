75415b318a71510a2c37fa84854c86ff77905eed
URK21CO2003 <110015503+URK21CO2003@users.noreply.github.com>
2022-08-09 18:24:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,98 +1,43 @@
-class Evaluate:
-  
-  
-
-  def __init__(self, size):
-
-   
-    self.top = -1
-    self.size_of_stack = size
-    self.stack = [None]*size
-
-
-  def isEmpty(self):
-    
-    Returns:
-    
-    return (self.top==-1)
-
-
-  def pop(self):
-
-   
-    Returns:
-     
-    if not self.isEmpty():
-
-      k=self.stack[self.top]
-      self.top-=1
-    return k
-
-
-  def push(self, operand):
-   
-    self.stack[self.top]=operand
-
-
-  def validate_postfix_expression(self, expression):
-    
-    operatorcon=0
-    operandcon=0
-    flag=0
-    for i in expression:
-
-      if(not(i=="-" or i=="+" or i=="*" or i=="/" or i=="^")):
-
-        k=int(i)
-      if(i=="-" or i=="+" or i=="*" or i=="/" or i=="^"):
-
-        operatorcon+=1
-        flag=1
-      elif(isinstance(k,int)):    
-        operandcon+=1
-        flag=1
-      else:
-        flag=0
-        break
-    if(flag==1 and operandcon==(operatorcon+1)):
-      return True
-    else:
-      return False
-
-
-  def evaluate_postfix_expression(self, expression):
-    
-    flag=0
-    for i in expression:
-
-      if(not(i=="-" or i=="+" or i=="*" or i=="/" or i=="^")):
-
-        self.push(i)
-      else:
-        self.push(i)
-        operator=self.pop()
-        secoperator=self.pop()
-        if(operator=="+"):
-
-          self.stack[self.top]=int(self.stack[self.top])+int(secoperator)
-        elif(operator=="-"):
-          self.stack[self.top]=int(self.stack[self.top])-int(secoperator)
-        elif(operator=="*"):
-          self.stack[self.top]=int(self.stack[self.top])*int(secoperator)
-        elif(operator=="/"):
-          flag=1
-          self.stack[self.top]=int(self.stack[self.top])/int(secoperator)
+class evaluate_postfix:
+    def __init__(self):
+        self.items=[]
+        self.size=-1
+    def isEmpty(self):
+        return self.items==[]
+    def push(self,item):
+        self.items.append(item)
+        self.size+=1
+    def pop(self):
+        if self.isEmpty():
+            return 0
         else:
-          self.stack[self.top]=int(self.stack[self.top])^int(secoperator)
-    
-    return int(self.stack[self.top])
-
-
-# Do not change the following code
-postfix_expression = input()  # Read postfix expression
-tokens = postfix_expression.split()
-evaluate = Evaluate(len(tokens))
-if evaluate.validate_postfix_expression(tokens):
-    print(evaluate.evaluate_postfix_expression(tokens))
-else:
-    print('Invalid postfix expression')
+            self.size-=1
+            return self.items.pop()
+    def seek(self):
+        if self.isEmpty():
+            return False
+        else:
+            return self.items[self.size]
+    def evalute(self,expr):
+        for i in expr:
+            if i in '0123456789':
+                self.push(i)
+            else:
+                op1=self.pop()
+                op2=self.pop()
+                result=self.cal(op2,op1,i)
+                self.push(result)
+        return self.pop()
+    def cal(self,op2,op1,i):
+        if i is '*':
+            return int(op2)*int(op1)
+        elif i is '/':
+            return int(op2)/int(op1)
+        elif i is '+':
+            return int(op2)+int(op1)
+        elif i is '-':
+            return int(op2)-int(op1)
+s=evaluate_postfix()
+expr=input('enter the postfix expression')
+value=s.evalute(expr)
+print('the result of postfix expression',expr,'is',value)
