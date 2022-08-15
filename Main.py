58963286cf3bf2a00fbf009f247c5ec344dc8b9c
URK21CO2003 <110015503+URK21CO2003@users.noreply.github.com>
2022-08-15 19:28:46 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,57 +1,43 @@
-def postfix_evaluation(s):
-
-    s=s.split()
-
-    n=len(s)
-
-    stack =[]
-
-    for i in range(n):
-
-        if s[i].isdigit():
-
-          #append function is equivalent to push
-
-            stack.append(int(s[i]))
-
-        elif s[i]=="+":
-
-            a=stack.pop()
-
-            b=stack.pop()
-
-            stack.append(int(a)+int(b))
-
-        elif s[i]=="*":
-
-            a=stack.pop()
-
-            b=stack.pop()
-
-            stack.append(int(a)*int(b))
-
-        elif s[i]=="/":
-
-            a=stack.pop()
-
-            b=stack.pop()
-
-            stack.append(int(b)/int(a))
-
-        elif s[i]=="-":
-
-            a=stack.pop()
-
-            b=stack.pop()
-
-            stack.append(int(b)-int(a))            
-
-    return stack.pop()
-
-#space separtor is required , for solving 2 or more digits .
-
-s="10 2 8 * + 3 -"
-
-val=postfix_evaluation(s)
-
-print(val)
+class evaluate_postfix:
+    def __init__(self):
+        self.items=[]
+        self.size=-1
+    def isEmpty(self):
+        return self.items==[]
+    def push(self,item):
+        self.items.append(item)
+        self.size+=1
+    def pop(self):
+        if self.isEmpty():
+            return 0
+        else:
+            self.size-=1
+            return self.items.pop()
+    def seek(self):
+        if self.isEmpty():
+            return False
+        else:
+            return self.items[self.size]
+    def evalute(self,expr):
+        for i in expr:
+            if i in '0123456789':
+                self.push(i)
+            else:
+                op1=self.pop()
+                op2=self.pop()
+                result=self.cal(op2,op1,i)
+                self.push(result)
+        return self.pop()
+    def cal(self,op2,op1,i):
+        if i is '*':
+            return int(op2)*int(op1)
+        elif i is '/':
+            return int(op2)/int(op1)
+        elif i is '+':
+            return int(op2)+int(op1)
+        elif i is '-':
+            return int(op2)-int(op1)
+s=evaluate_postfix()
+expr=input('enter the postfix expression')
+value=s.evalute(expr)
+print('the result of postfix expression',expr,'is',value)
