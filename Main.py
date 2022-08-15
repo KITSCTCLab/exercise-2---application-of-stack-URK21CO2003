b9f7a1c3a2d8a94a8d4a0a4b3c7730e749e9e232
URK21CO2003 <110015503+URK21CO2003@users.noreply.github.com>
2022-08-15 19:14:10 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,39 +1,57 @@
-def evaluatePostfix(givenExp):
-    # Create a stack by taking an empty list which acts
-    # as a stack in this case to hold operands (or values).
-    givenstack = []
-    # Traverse the given postfix expression using For loop.
-    for charact in givenExp:
-        # Push the element into the given stack if it is a number.
-        if charact.isdigit():
-            givenstack.append(int(charact))
-        # if the character is operator
-        else:
-            # remove the top two elements from the stack
-            topfirst = givenstack.pop()
-            topsecond = givenstack.pop()
-            # Evaluate the operator and return the answer to the stack using append() funtion.
-            # If the operator is '+' then perform an addition operation on
-            # the top two elements by popping them out.
-            if charact == '+':
-                givenstack.append(topsecond + topfirst)
-            # If the operator is '-' then perform a subtraction operation
-            # on the top two elements by popping them out.
-            elif charact == '-':
-                givenstack.append(topsecond - topfirst)
-            # If the operator is '/' then perform a division operation on
-            # the top two elements by popping them out.
-            elif charact == '×':
-                givenstack.append(topsecond * topfirst)
-            # If the operator is '*' then perform a multiplication operation
-            # on the top two elements by popping them out.
-            elif charact == '/':
-                givenstack.append(topsecond // topfirst)
-    # The only number in the stack is the final answer when the expression is traversed.
-    # return the answer to the main function
-    return givenstack.pop()
-# Driver code
-# Give the postfix Expression as user input using input() function and store it in a variable.
-givenExp = input('Enter some random postfix Expression = ')
-# Pass the given postfix Expression as an argument to evalpostfix function
-print('The value of the given postfix expression =', evaluatePostfix(givenExp))
+def postfix_evaluation(s):
+
+    s=s.split()
+
+    n=len(s)
+
+    stack =[]
+
+    for i in range(n):
+
+        if s[i].isdigit():
+
+          #append function is equivalent to push
+
+            stack.append(int(s[i]))
+
+        elif s[i]=="+":
+
+            a=stack.pop()
+
+            b=stack.pop()
+
+            stack.append(int(a)+int(b))
+
+        elif s[i]=="*":
+
+            a=stack.pop()
+
+            b=stack.pop()
+
+            stack.append(int(a)*int(b))
+
+        elif s[i]=="/":
+
+            a=stack.pop()
+
+            b=stack.pop()
+
+            stack.append(int(b)/int(a))
+
+        elif s[i]=="-":
+
+            a=stack.pop()
+
+            b=stack.pop()
+
+            stack.append(int(b)-int(a))            
+
+    return stack.pop()
+
+#space separtor is required , for solving 2 or more digits .
+
+s="10 2 8 * + 3 -"
+
+val=postfix_evaluation(s)
+
+print(val)
